ee5c08bb944e9b96aacd6612115cf5a781e7f109
Mohd-Afroz-2 <afrozaffu2786@gmail.com>
2026-02-07 21:34:23 +0530

    Add files via upload

---
+++ timer/timer.js
@@ -0,0 +1,40 @@
+const start = document.getElementById("start")
+const stop = document.getElementById("stop")
+const reset = document.getElementById("reset")
+const timer = document.getElementById("timer")
+
+let timeLeft = 1500;
+let interval;
+
+const updateTimer = () => {
+    const minutes =  Math.floor(timeLeft / 60); 
+    const seconds = timeLeft % 60 ;
+
+    let displayMinutes = minutes < 10 ?"0" + minutes : minutes ;
+    let displaySeconds = seconds < 10 ?"0" + seconds : seconds ;
+
+    timer.innerHTML=`${displayMinutes} : ${displaySeconds}`;
+}
+
+const startTimer = () => {
+    interval = setInterval (()=>{
+        timeLeft--;
+        updateTimer();
+
+        if (timeLeft === 0) {
+            clearInterval(interval)
+            alert("Time's up...stop !!!!");
+            timeLeft=1500;
+            updateTimer();
+        }
+    },  
+    1000 )// for every second
+}
+
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer =()=>{
+    clearInterval(interval);
+    timeLeft=1500;
+    updateTimer();
+}
